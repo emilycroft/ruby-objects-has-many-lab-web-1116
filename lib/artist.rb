36da7d5b8c2b18d08e6d08b816b063ea819a5aba
emilycroft <emilyhcroft@gmail.com>
2016-12-02 09:02:16 -0500

    done

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name
+  attr_reader :songs
+
+
+  @@song_count = 0
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(name)
+    new_song = Song.new(name)
+    new_song.artist = self
+    @songs << new_song
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+end
